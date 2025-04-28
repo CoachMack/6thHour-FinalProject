697ecdf732f351f2fced4462b20678f7ee292879
Logan Zimmerman <lz128152@student.wpsok.org>
2025-04-28 14:04:15 -0500

    added final project

--- Team 2 Final Project.py
+++ Team 2 Final Project.py
@@ -31,15 +31,11 @@ class Character:
             print(f"character takes {damage_taken} damage. New health: {self.health}")
             time.sleep(1)
 
-    def mp_used(self):
+    def magic_used(self):
         mp_used = random.randint(1, 10)
         self.mp -= mp_used
-        print(f"character has {self.mp} left")
+        print(f"character has {self.magic} left")
         time.sleep(1)
-
-    def attack(self):
-        if Character.stamina < 0:
-            enemy1.take_damage()
 
     def dmgBuff(self):
         self.damage += 50
@@ -61,7 +57,7 @@ guard = Character(750, 100, 50, 90, 175, 20, 750, 0)
 def first_apperance():
     print('Hello welcome to your adventure')
     name = input('Who would you like to be known as: ')
-    choice = int(input('ok', name, 'now what would you like to be, warrior(1) thief(2) or wizard(3): '))
+    choice = int(input('ok now what would you like to be, warrior(1) thief(2) or wizard(3): '))
     if choice == 1:
         warriorJourney()
     if choice == 2:
@@ -71,12 +67,31 @@ def first_apperance():
 
 
 def warriorJourney():
+    print('You start at the kingdom as a kings knight but once you return to your home you get a letter from a neighboring kingdom that needs your help and will reward you with glory and power')
+    acception = int(input('do you want to accept(1) or decline(2): '))
+
+    if acception == 1:
+        global name
+        print('you pack up your stuff and plan to leave by morning')
+        time.sleep(2)
+        print('you go to sleep but wake up to see an unknown dark figure above you, it bursts out the door but you grab you stuff and run out to chase it but once you get out its gone')
+        time.sleep(7)
+        print('an old friend sees your panic, "hey',name,' whats the rush, are you ok", you say your fine and head off to the kingdom with nothing but the clothes on your back and a simple sword')
+        time.sleep(7)
 
 
+
+    #ending number 1
+    elif acception == 2:
+        print('You stayed at your kingdom to serve your king and stay loyal till you die')
+        exit()
+    else:
+        print('not an option, pick 1 or 2')
+        warriorJourney()
 def thiefJourney():
-
+    print()
 
 def wizardJourney():
-
+    print()
 
 first_apperance()
